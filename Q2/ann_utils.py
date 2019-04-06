1c27ef26c1a3add9d01ac0ebebed9f8aade0a16d
pkumar2618 <pawan.kumar@cse.iitd.ac.in>
2019-04-06 22:23:46 +0530

    better relu

--- Q2/ann_utils.py
+++ Q2/ann_utils.py
@@ -220,13 +220,37 @@ def linear_backward(dZ, cache):
     A_prev, W, b = cache
     m = A_prev.shape[1]
 
+
+    dW = np.dot(dZ, A_prev.T)
+    db = np.sum(dZ, axis=1, keepdims=True)
+    dA_prev = np.dot(W.T, dZ)
+
+    assert (dA_prev.shape == A_prev.shape)
+    assert (dW.shape == W.shape)
+    assert (db.shape == b.shape)
+
+    return dA_prev, dW, db
+
+def linear_backward_relu(dZ, cache):
+    """
+    linear portion of backward prop for a single layer (layer l)
+
+    Arguments:
+    dZ -- Gradient of the cost with respect to the linear output (of current layer l)
+    cache -- tuple of values (A_prev, W, b) coming from the forward propagation in the current layer
+
+    Returns:
+    dA_prev -- Gradient of the cost with respect to the activation (of the previous layer l-1), same shape as A_prev
+    dW -- Gradient of the cost with respect to W (current layer l), same shape as W
+    db -- Gradient of the cost with respect to b (current layer l), same shape as b
+    """
+    A_prev, W, b = cache
+    m = A_prev.shape[1]
+
     dW = 1. / m * np.dot(dZ, A_prev.T)
     db = 1. / m * np.sum(dZ, axis=1, keepdims=True)
     dA_prev = np.dot(W.T, dZ)
 
-    # dW = np.dot(dZ, A_prev.T)
-    # db = np.sum(dZ, axis=1, keepdims=True)
-    # dA_prev = np.dot(W.T, dZ)
 
     assert (dA_prev.shape == A_prev.shape)
     assert (dW.shape == W.shape)
@@ -253,7 +277,7 @@ def linear_activation_backward(dA, cache, activation):
 
     if activation == "relu":
         dZ = relu_backward(dA, activation_cache)
-        dA_prev, dW, db = linear_backward(dZ, linear_cache)
+        dA_prev, dW, db = linear_backward_relu(dZ, linear_cache)
 
     elif activation == "sigmoid":
         dZ = sigmoid_backward(dA, activation_cache)
